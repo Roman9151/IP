cccb0c98cafae217dcda211b3d7e8b5fa21774f3
ADM <romanos1239@gmail.com>
2023-07-24 09:53:03 +0300

    using cats-retry

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.3.0"
 val circeVersion = "0.14.3"
 val http4sVersion = "0.23.15"
+val catsRetryVersion = "3.1.0"
 libraryDependencies += "com.comcast" %% "ip4s-core" % "3.2.0"
 libraryDependencies ++= Seq(
   "io.circe" %% "circe-core",
@@ -13,11 +14,13 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
   "org.http4s" %% "http4s-circe" % http4sVersion,
 )
+
+libraryDependencies += "com.github.cb372" %% "cats-retry" % catsRetryVersion
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.16"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.16" % "test"
 ThisBuild / libraryDependencies += "org.slf4j" % "slf4j-simple" % "2.0.7"
 assemblyMergeStrategy in assembly := {
-  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case PathList("META-INF", xs@_*) => MergeStrategy.discard
   case x => MergeStrategy.first
 }
 lazy val root = (project in file("."))
--- src/main/scala/org/ipify/getter/Main.scala
+++ src/main/scala/org/ipify/getter/Main.scala
@@ -1,5 +1,6 @@
 package org.ipify.getter
 
+import scala.concurrent.duration.*
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits.*
 import com.comcast.ip4s.IpAddress
@@ -8,16 +9,39 @@ import org.http4s.client.Client
 import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.implicits.uri
 import org.ipify.getter.IPResponse.*
+import retry.*
+import retry.RetryDetails.{GivingUp, WillDelayAndRetry}
+
+import java.io.IOException
 
 object Main extends IOApp.Simple {
   private val APIFY = uri"https://api.ipify.org/?format=json"
 
+  private val retryPolicyWithLimit = RetryPolicies.limitRetries[IO](4)
+  private val retryPolicyWithDelay = RetryPolicies.constantDelay[IO](500.millis)
+  private val limitWithDelayPolicy = retryPolicyWithLimit.join(retryPolicyWithDelay)
+
+  private def logError(err: Throwable, details: RetryDetails): IO[Unit] = details match {
+    case WillDelayAndRetry(nextDelay: FiniteDuration,
+    retriesSoFar: Int,
+    cumulativeDelay: FiniteDuration) =>
+      IO.println(s"Failed to execute api call due to ${err.getMessage}. So far we have retried $retriesSoFar times.")
+    case GivingUp(totalRetries: Int, totalDelay: FiniteDuration) =>
+      IO.println(s"Giving up after $totalRetries retries")
+  }
+
+  private def checkIOException(t: Throwable): IO[Boolean] = t match
+    case _: IOException => IO.pure(true)
+    case _ => IO.pure(false)
+
   private def getIP(client: Client[IO]): IO[IPResponse] = {
-    client
+    val res = client
       .expect[IPResponse](APIFY)
-      .handleErrorWith { error =>
-        IO.raiseError(new RuntimeException(s"Failed to get IP: ${error.getMessage}"))
-      }
+    retryingOnSomeErrors[IPResponse](
+      policy = limitWithDelayPolicy,
+      isWorthRetrying = checkIOException,
+      onError = logError
+    )(res)
   }
 
   private def execute: IO[IPResponse] = BlazeClientBuilder
