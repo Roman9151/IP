96e3d0f0e65b3226efefebd514db43945216a58f
ADM <romanos1239@gmail.com>
2023-07-29 14:06:33 +0300

    changed ember client to blaze client

--- src/main/scala/org/ipify/getter/Main.scala
+++ src/main/scala/org/ipify/getter/Main.scala
@@ -5,28 +5,27 @@ import cats.implicits.*
 import com.comcast.ip4s.IpAddress
 import io.circe.parser.decode
 import org.http4s.client.Client
-import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.implicits.uri
-import org.ipify.getter.IPResponse
 import org.ipify.getter.IPResponse.*
 
 object Main extends IOApp.Simple {
-  private val IPIFY = uri"https://api.ipify.org/?format=json"
+  private val APIFY = uri"https://api.ipify.org/?format=json"
 
   private def getIP(client: Client[IO]): IO[IPResponse] = {
     client
-      .expect[IPResponse](IPIFY)
+      .expect[IPResponse](APIFY)
       .handleErrorWith { error =>
         IO.raiseError(new RuntimeException(s"Failed to get IP: ${error.getMessage}"))
       }
   }
 
-  private def execute: IO[IPResponse] = EmberClientBuilder
-    .default[IO]
-    .build
+  private def execute: IO[IPResponse] = BlazeClientBuilder
+    .apply[IO]
+    .resource
     .use(getIP)
 
 
-  override def run: IO[Unit] = execute.flatMap(IO.println)
+  override def run: IO[Unit] = execute.flatMap(response => IO.println(response.ip)).as(ExitCode.Success)
 
 }
